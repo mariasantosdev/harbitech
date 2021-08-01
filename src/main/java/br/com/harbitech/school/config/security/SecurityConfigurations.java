9cf6d044af68ed3e925ad2c98bca821da27d9f70
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-08-01 11:20:41 -0300

    Começando a fazer a configuração do Spring Security no projeto

---
+++ src/main/java/br/com/harbitech/school/config/security/SecurityConfigurations.java
@@ -0,0 +1,10 @@
+package br.com.harbitech.school.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+}
